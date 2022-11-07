0801d32d325815adf081b39c3e5b66285e290afb
Beka Shengelia <bekashengelia333@gmail.com>
2022-11-07 10:22:55 +0400

    write with streams

--- public/app.js
+++ public/app.js
@@ -3,8 +3,9 @@ let http = require('http');
 let fs = require('fs');
 
 let myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf-8');
+let myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
 myReadStream.on('data', function(chunk){
     console.log('new chunk recieved')
-    console.log(chunk)
+    myWriteStream.write(chunk)
 })
