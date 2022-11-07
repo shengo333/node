b1c70af9e541e74d515bf1e89d0612b758704205
Beka Shengelia <bekashengelia333@gmail.com>
2022-11-07 10:44:32 +0400

    pipe to server res

--- public/app.js
+++ public/app.js
@@ -2,10 +2,17 @@ let http = require('http');
 
 let fs = require('fs');
 
-let myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf-8');
-let myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
-myReadStream.on('data', function(chunk){
-    console.log('new chunk recieved')
-    myWriteStream.write(chunk)
+
+
+var server = http.createServer(function(req,res){
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    let myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf-8');
+
+    myReadStream.pipe(res)
 })
+
+
+server.listen(3000, '127.0.0.1')
+console.log('yo dawgs, now listening to port 3000')
