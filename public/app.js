b4da459ffee579d87ec4537a0dc3374d0f2715ad
Beka Shengelia <bekashengelia333@gmail.com>
2022-11-08 16:07:50 +0400

    first appearence

--- public/app.js
+++ public/app.js
@@ -7,8 +7,8 @@ let fs = require('fs');
 
 var server = http.createServer(function(req,res){
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    let myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf-8');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf-8');
 
     myReadStream.pipe(res)
 })
