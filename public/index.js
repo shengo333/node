e53a6c0ee7bd7387cca5c957b375aea06b5331cc
Beka Shengelia <bekashengelia333@gmail.com>
2022-11-12 11:04:34 +0400

    route params

--- public/index.js
+++ public/index.js
@@ -10,4 +10,9 @@ app.get('/contact', function (req, res){
   res.send('this is contact page')
 });
 
+app.get('/profile/:id', function(req, res){
+  res.send('you id is ' + req.params.id)
+});
+
+
 app.listen(3000);
