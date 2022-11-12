b8dcbad8408f2b6d58283aab3a70710819f4abc6
Beka Shengelia <bekashengelia333@gmail.com>
2022-11-12 11:43:13 +0400

    node dynamic data

---
+++ index.js
@@ -0,0 +1,20 @@
+let express = require('express');
+let app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/contact', function (req, res){
+  res.sendFile(__dirname + '/contact.html')
+});
+
+app.get('/profile/:name', function(req, res){
+  let data = {age: 29, job: 'ninja'};
+  res.render('profile', {person: req.params.name, data: data})
+});
+
+
+app.listen(3000);
