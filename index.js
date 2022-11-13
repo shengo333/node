94041b6df9fd045afd37ee0213b818d303b9c3f7
Beka Shengelia <bekashengelia333@gmail.com>
2022-11-13 17:52:24 +0400

    Middleware

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let express = require('express');
 let app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'))
 
 app.get('/', function (req, res){
   res.render('index')
