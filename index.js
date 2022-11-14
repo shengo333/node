8165e7369238976ee6a42da5542791812543acfe
Beka Shengelia <bekashengelia333@gmail.com>
2022-11-14 09:48:34 +0400

    query strings

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.get('/', function (req, res){
 });
 
 app.get('/contact', function (req, res){
-  res.render('contact')
+  console.log(req.query)
+  res.render('contact',{qs: req.query})
 });
 
 app.get('/profile/:name', function(req, res){
