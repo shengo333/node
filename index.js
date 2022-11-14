e6f9ccfeb7906c9313f32f26a069e444f90f69ff
Beka Shengelia <bekashengelia333@gmail.com>
2022-11-14 17:13:08 +0400

    body parse

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'))
@@ -11,6 +14,10 @@ app.get('/', function (req, res){
 app.get('/contact', function (req, res){
   console.log(req.query)
   res.render('contact',{qs: req.query})
+});
+app.post('/contact', urlencodedParser, function (req, res){
+  console.log(req.body)
+  res.render('contact-success',{data: req.body})
 });
 
 app.get('/profile/:name', function(req, res){
